4f196b5a4581b77d718242e5478ab9b3dcb75376
Bhiravina Mahesaratnam <bhiravina@hotmail.com>
2024-08-30 10:58:25 +0200

    La til data i tabeller

--- buildDatabase.sql
+++ buildDatabase.sql
@@ -29,3 +29,20 @@ CREATE TABLE Podkast (
 	FOREIGN KEY (SpraakID) REFERENCES Spraak(ID),
 	FOREIGN KEY (UtgiverID) REFERENCES Utgiver(ID)
 );
+
+INSERT INTO Kategori VALUES 
+(DEFAULT, 'Helse'),
+(DEFAULT, 'Tech'),
+(DEFAULT, 'Reise');
+
+INSERT INTO Spraak VALUES
+(DEFAULT, 'Engelsk'),
+(DEFAULT, 'Norsk');
+
+INSERT INTO Utgiver VALUES
+(DEFAULT, 'Per Hansen'),
+(DEFAULT, 'Ingrid Moe');
+
+INSERT INTO Podkast VALUES
+(DEFAULT, 1, 1, 2, 'Keto diett', 'Beskrivelse av keto', 'www.bildeUrl/bilde.jpeg'),
+(DEFAULT, 3, 2, 1, 'Nyttige tips for bestilling av flybiletter', 'Beskrivelse av tips', 'www.bildeUrl/bilde2.jpeg');
